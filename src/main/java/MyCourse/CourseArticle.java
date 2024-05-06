bfd350c727722c4abcb4ca982071c71787f1d594
Harishpsy <harishpsy46@gmail.com>
2024-05-06 18:39:24 +0530

    updated

--- src/main/java/MyCourse/CourseArticle.java
+++ src/main/java/MyCourse/CourseArticle.java
@@ -1,12 +1,14 @@
 package MyCourse;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.openqa.selenium.By.xpath;
+import static org.openqa.selenium.By.*;
 
 public class CourseArticle {
     WebDriver driver;
@@ -37,26 +39,26 @@ public class CourseArticle {
         WebElement clickingArticleTab = driver.findElement (xpath ("//*[text()='Articles']"));
         clickingArticleTab.click ();
 
-//       // Find elements using either XPath or loop through individual cards
-//
-//       final List<WebElement> mainsCards = driver.findElements(xpath("//*[text()='GA Questions Asked in SBI Clerk Mains Exam 2023 - 2024']"));
-//       final List<WebElement> prelimsCards = driver.findElements(xpath("//*[text()='SBI Clerk Prelims Exam 2023 Quants - Self Analysis']"));
-//
-//      // Verification (assuming you have a way to verify card existence)
-//
-//        if (mainsCards.isEmpty()) {
-//            System.out.println("Verification failed: 'GA Questions...' cards not found.");
-//        } else {
-//            System.out.println("Found 'GA Questions...' cards.");
-//        }
-//
-//        if (prelimsCards.isEmpty()) {
-//            System.out.println("Verification failed: 'SBI Clerk Prelims...' cards not found.");
-//        } else {
-//            System.out.println("Found 'SBI Clerk Prelims...' cards.");
-//        }
+        // Find elements using either XPath or loop through individual cards
 
-        //  Method for Counting How many Cards present
+        final List<WebElement> mainsCards = driver.findElements (xpath ("//*[text()='GA Questions Asked in SBI Clerk Mains Exam 2023 - 2024']"));
+        final List<WebElement> prelimsCards = driver.findElements (xpath ("//*[text()='SBI Clerk Prelims Exam 2023 Quants - Self Analysis']"));
+
+        // Verification (assuming you have a way to verify card existence)
+
+        if (mainsCards.isEmpty ()) {
+            System.out.println ("Verification failed: 'GA Questions...' cards not found.");
+        } else {
+            System.out.println ("Found 'GA Questions...' cards.");
+        }
+
+        if (prelimsCards.isEmpty ()) {
+            System.out.println ("Verification failed: 'SBI Clerk Prelims...' cards not found.");
+        } else {
+            System.out.println ("Found 'SBI Clerk Prelims...' cards.");
+        }
+
+        //  Method for Counting How many Cards present in the list
 
 
 //        List<WebElement> articleCards = new ArrayList<> ();
@@ -77,62 +79,64 @@ public class CourseArticle {
 
         // Clicking the three Dots in the CourseCard
 
+        Thread.sleep (3000);
         WebElement clickingThreeDots = driver.findElement (xpath ("//*[@class=\"anticon anticon-more\"]"));
         clickingThreeDots.click ();
 
-        // Clicing The save My Notes
+        // Clicking The save My Notes or Remove my notes
 
         Thread.sleep (3000);
-        WebElement clickingSaveMyNotes = driver.findElement (xpath ("//*[text()='Save to My Notes']"));
-        clickingSaveMyNotes.click ();
-        System.out.println ("Sucessfully - Saved - Article");
+        WebElement clickMyNote = driver.findElement (xpath ("//*[contains(text(), 'Save to My Notes') or contains(text(), 'Remove My  Notes')]"));
 
-//
-//        WebElement SaveMynotes = driver.findElement (xpath ("//*[text()='Save to My Notes']"));
-//
-//
-//        if (SaveMynotes.isDisplayed())
-//        {
-//            SaveMynotes.click ();
-//            System.out.println("Verification paused: Sucessfully clicked SaveMyNotes.");
-//        }
-//     else
-//        {
-//            WebElement clickingRemoveMyNotes = driver.findElement (xpath ("//*[text()='Remove My  Notes']"));
-//            clickingRemoveMyNotes.click ();
-//            System.out.println("Verification Failed: Both Not Clicked.");
-//
-//        }
+        if (clickMyNote.isDisplayed ()) {
+            clickMyNote.click ();
+
+            if (clickMyNote.getText ().contains ("Remove My  Notes")) {
+                System.out.println ("Successfully - Removed - Article");
+            } else {
+                System.out.println ("Successfully - Saved - Article");
+            }
+        }
 
         // Clicking The home button
 
+        Thread.sleep (3000);
         WebElement clickingHomeButton = driver.findElement (xpath ("//*[text()='Home']"));
         clickingHomeButton.click ();
 
         // Clicking The MyNotes
 
+        Thread.sleep (3000);
         WebElement clickingMyNotes = driver.findElement (xpath ("//*[@id=\"1\"]"));
         clickingMyNotes.click ();
 
-        // Verifying the saved Notes was showing in MyNotes page
+        // Verifying the article in my notes
 
         final List<WebElement> savedarticle = driver.findElements (xpath ("//*[@src=\"https://assets.careerscloud.in/course/article/images/57f0d0c460019c05a8f66e6a9b3476d0.webp\"]"));
 
         // Verification (assuming you have a way to verify card existence)
 
         if (savedarticle.isEmpty ()) {
+
             System.out.println ("Verification failed: 'Article was not present...' cards not found.");
+
         } else {
-            System.out.println ("Verification Passed:  'Article present...' cards.");
+
+            System.out.println ("Verification Passed: 'Article present...' cards.");
         }
+
+//        final List<WebElement> savedArticle = driver.findElements (xpath ("//img[@src='https://assets.careerscloud.in/course/article/images/57f0d0c460019c05a8f66e6a9b3476d0.webp']"));
+//
+//        // Verification (using assertion)
+//
+//        Assert.assertEquals (savedArticle.isEmpty (), "Verification failed: 'Article was not present...' ");
+//        System.out.println ("Verification Passed: 'Article present...'.");
 
         // Clicking The My Course Button
 
         driver.manage ().timeouts ().implicitlyWait (Duration.ofSeconds (30));
         clickingMyCourseButton = driver.findElement (xpath ("//* [text()='My Course']"));
         clickingMyCourseButton.click ();
-
-        // Clcking Course
 
         // Clicking The Eigth Course Card In the My Course
 
@@ -142,44 +146,91 @@ public class CourseArticle {
 
         // Clicking The Article
 
+        Thread.sleep (3000);
         clickingArticleTab = driver.findElement (xpath ("//*[text()='Articles']"));
         clickingArticleTab.click ();
 
-        // Clicking the three Dots in the CourseCard
-
-        clickingThreeDots = driver.findElement (xpath ("//*[@class=\"anticon anticon-more\"]"));
-        clickingThreeDots.click ();
-
-        // Clicing The Remove My Notes
+        // Clicking the view Article
 
         Thread.sleep (3000);
-        WebElement clickingRemoveMyNotes = driver.findElement (xpath ("//*[text()='Remove My  Notes']"));
-        clickingRemoveMyNotes.click ();
-        System.out.println ("Sucessfully - Removed - Article");
+        WebElement clickingViewButton = driver.findElement (xpath ("//*[@class=\"ant-btn css-xu9wm8 ant-btn-ghost ant-btn-block card-view-button\"]"));
+        clickingViewButton.click ();
 
-        // Clicking The home button
+        // Clicking the like and unlike button
 
-        clickingHomeButton = driver.findElement (xpath ("//*[text()='Home']"));
-        clickingHomeButton.click ();
+        WebElement clickAction = driver.findElement (xpath ("//*[contains(@class, 'like') or contains(@class, 'unlike')]"));
 
-        // Clicking The MyNotes
+        if (clickAction.isDisplayed ()) {
+            clickAction.click ();
 
-        clickingMyNotes = driver.findElement (xpath ("//*[@id=\"1\"]"));
-        clickingMyNotes.click ();
 
-        // Verifying the Article Removed Notes was showing in MyNotes page
-
-        final List<WebElement> Removedarticle = driver.findElements (xpath ("//*[@src=\"https://assets.careerscloud.in/course/article/images/57f0d0c460019c05a8f66e6a9b3476d0.webp\"]"));
-
-        // Verification (assuming you have a way to verify card existence)
-
-        if (Removedarticle.isEmpty ()) {
-            System.out.println ("Verification failed: 'Article was not present...' cards not found.");
-        } else {
-            System.out.println ("Verification Passed:  'Article present...' cards.");
+            if (clickAction.getAttribute ("class").contains ("unlike")) {
+                System.out.println (" Un Liked Successfully ");
+            } else {
+                System.out.println (" Liked Successfully ");
+            }
         }
 
+        // Clicking The Comment Icon
 
+        WebElement clickingCommentIcon = driver.findElement (id ("comments-icon"));
+        clickingCommentIcon.click ();
+
+        // Entering The comments
+
+        Thread.sleep (3000);
+        WebElement enteringComments = driver.findElement (name ("comments1"));
+        enteringComments.sendKeys ("Thanks For the update");
+        System.out.println ("Comment Added Sucessfully");
+
+        // Clicking The send Button
+
+        Thread.sleep (3000);
+        WebElement clickingSendButton = driver.findElement (xpath ("//*[@class=\"anticon anticon-send\"]"));
+        clickingSendButton.click ();
+
+        // Clicking Float button
+
+        WebElement clickingFloatButton = driver.findElement (xpath ("//*[@class=\"css-xu9wm8 ant-float-btn ant-float-btn-default ant-float-btn-circle\"]"));
+        clickingFloatButton.click ();
+
+        // Clicking the share icon
+
+        Thread.sleep (3000);
+        WebElement clickingshareicon = driver.findElement (xpath ("//*[@class=\"share\"]"));
+        clickingshareicon.click ();
+
+        //click the copy link in the share popup
+
+        Thread.sleep (3000);
+        WebElement Copy_link = driver.findElement (xpath ("//span[text()='COPY LINK']"));
+        Copy_link.click ();
+
+        //Clicking the cancel button in the in share popup
+
+        Thread.sleep (3000);
+        WebElement Cancel_button = driver.findElement (xpath ("//span[text()='Cancel']"));
+        Cancel_button.click ();
+        System.out.println ("Clicked cancel button");
+
+        // Verifying the Recent article
+
+        String[] articleLinks = {"/course-details/62/article/3758", "/course-details/62/article/3586", "/course-details/62/article/3557", "/course-details/62/article/3510", "/course-details/62/article/3408"};
+
+        List<WebElement> recentArticle = driver.findElements (tagName ("a"));
+
+        int count = 2;
+
+        for (WebElement recentArticles : recentArticle) {
+
+            String actuallinks = recentArticles.getAttribute ("href");
+
+            if (Arrays.asList (articleLinks).contains (actuallinks)) {
+                count++;
+                System.out.println ("All the article was present");
+            }
+        }
+        Assert.assertEquals (count, articleLinks.length);
 
 
 
