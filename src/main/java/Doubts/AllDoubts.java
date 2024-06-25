ff7d86af2d796fe22f5442594a52c96fa5e352e3
Harishpsy <harishpsy46@gmail.com>
2024-06-25 18:49:26 +0530

    updated

--- src/main/java/Doubts/AllDoubts.java
+++ src/main/java/Doubts/AllDoubts.java
@@ -1,18 +1,16 @@
 package Doubts;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-import static org.openqa.selenium.By.*;
+import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.By.xpath;
 
 public class AllDoubts {
 
@@ -25,58 +23,58 @@ public class AllDoubts {
 
     public void allDoubts() throws AWTException, InterruptedException {
 
-        // Clicking The Create Doubt Button
-
-        WebElement clickingCreateButton = driver.findElement (xpath ("//*[@class=\"ant-btn css-xu9wm8 ant-btn-primary add-doubts-btn\"]"));
-        clickingCreateButton.click ();
-
-
-        // Clicking the search textfield and searching the course
-
-        Thread.sleep (5000);
-        WebElement clickingSearch = driver.findElement (xpath ("(//*[@name=\"comments2\"])[2]"));
-        clickingSearch.sendKeys ("Mock");
-
-        // Pressing The KeyBoard Action
-
-        Robot robot = new Robot ();
-
-        // clicking The Enter Button
-
-        Thread.sleep (3000);
-        robot.keyPress (KeyEvent.VK_ENTER);
-
-        // Releasing The Enter Button
-
-        Thread.sleep (5000);
-        robot.keyRelease (KeyEvent.VK_ENTER);
-
-        // Pressing Backspace Button In The Keyboard
-
-        // Set the number of times to perform the action
-
-        int numberOfTimes = 4;
-
-        // Creating a Loop to performe this action multiple times
-
-        for (int i = 0; i < numberOfTimes; i++) {
-            Thread.sleep (3000);
-            robot.keyPress (KeyEvent.VK_BACK_SPACE);
-
-            // Releasing the BackSpace
-
-            robot.keyRelease (KeyEvent.VK_BACK_SPACE);
-        }
-
-        // Pressing The Enter Button In The Keyboard
-
-        Thread.sleep (3000);
-        robot.keyPress (KeyEvent.VK_ENTER);
-
-        // Releassing The Enter Button In The Keyboard
-
-        robot.keyRelease (KeyEvent.VK_ENTER);
-        System.out.println ("SucessFully Released The Enter Button");
+//        // Clicking The Create Doubt Button
+//
+//        WebElement clickingCreateButton = driver.findElement (xpath ("//*[@class=\"ant-btn css-xu9wm8 ant-btn-primary add-doubts-btn\"]"));
+//        clickingCreateButton.click ();
+//
+//
+//        // Clicking the search textfield and searching the course
+//
+//        Thread.sleep (5000);
+//        WebElement clickingSearch = driver.findElement (xpath ("(//*[@name=\"comments2\"])[2]"));
+//        clickingSearch.sendKeys ("Mock");
+//
+//        // Pressing The KeyBoard Action
+//
+//        Robot robot = new Robot ();
+//
+//        // clicking The Enter Button
+//
+//        Thread.sleep (3000);
+//        robot.keyPress (KeyEvent.VK_ENTER);
+//
+//        // Releasing The Enter Button
+//
+//        Thread.sleep (5000);
+//        robot.keyRelease (KeyEvent.VK_ENTER);
+//
+//        // Pressing Backspace Button In The Keyboard
+//
+//        // Set the number of times to perform the action
+//
+//        int numberOfTimes = 4;
+//
+//        // Creating a Loop to performe this action multiple times
+//
+//        for (int i = 0; i < numberOfTimes; i++) {
+//            Thread.sleep (3000);
+//            robot.keyPress (KeyEvent.VK_BACK_SPACE);
+//
+//            // Releasing the BackSpace
+//
+//            robot.keyRelease (KeyEvent.VK_BACK_SPACE);
+//        }
+//
+//        // Pressing The Enter Button In The Keyboard
+//
+//        Thread.sleep (3000);
+//        robot.keyPress (KeyEvent.VK_ENTER);
+//
+//        // Releassing The Enter Button In The Keyboard
+//
+//        robot.keyRelease (KeyEvent.VK_ENTER);
+//        System.out.println ("SucessFully Released The Enter Button");
 
 //        // Focucing the popup
 //
@@ -91,23 +89,23 @@ public class AllDoubts {
 //        jse.executeScript ("window.scrollTo(0,document.body.scrollHeight)");
 //        System.out.println ("SucessFully Released The Enter Button");
 
-        // Clicking the Course to Create the Doubt
-
-        Thread.sleep (5000);
-        WebElement clickingCourse = driver.findElement (xpath ("(//*[@class=\"doubt2\"])[1]"));
-        clickingCourse.click ();
-
-        // Clicking the post-Doubts
-
-        Thread.sleep (5000);
-        WebElement clickingPostDoubts = driver.findElement (xpath ("//*[text()=\"Post Doubts\"]"));
-        clickingPostDoubts.click ();
-
-        // Entering The Text
-
-        Thread.sleep (5000);
-        WebElement enteringText = driver.findElement (xpath ("//*[@placeholder=\"Write your Doubts & Just paste(Ctrl+v) your cropped image here\"]"));
-        enteringText.sendKeys ("Where Can I Buy The Crack Current Affairs");
+//        // Clicking the Course to Create the Doubt
+//
+//        Thread.sleep (5000);
+//        WebElement clickingCourse = driver.findElement (xpath ("(//*[@class=\"doubt2\"])[1]"));
+//        clickingCourse.click ();
+//
+//        // Clicking the post-Doubts
+//
+//        Thread.sleep (5000);
+//        WebElement clickingPostDoubts = driver.findElement (xpath ("//*[text()=\"Post Doubts\"]"));
+//        clickingPostDoubts.click ();
+//
+//        // Entering The Text
+//
+//        Thread.sleep (5000);
+//        WebElement enteringText = driver.findElement (xpath ("//*[@placeholder=\"Write your Doubts & Just paste(Ctrl+v) your cropped image here\"]"));
+//        enteringText.sendKeys ("Where Can I Buy The Crack Current Affairs");
 
 //        // Clicking the upload Buttton
 //
@@ -148,11 +146,11 @@ public class AllDoubts {
 //        Thread.sleep (3000);
 //        robot.keyRelease (KeyEvent.VK_ENTER);
 
-        // Clicking the close Button
-
-        Thread.sleep (5000);
-        WebElement clickingCancelButton = driver.findElement (xpath ("(//*[@alt=\"Close\"])[2]"));
-        clickingCancelButton.click ();
+//        // Clicking the close Button
+//
+//        Thread.sleep (5000);
+//        WebElement clickingCancelButton = driver.findElement (xpath ("(//*[@alt=\"Close\"])[2]"));
+//        clickingCancelButton.click ();
 
 //        // Clicking The send-Button
 //
@@ -160,31 +158,31 @@ public class AllDoubts {
 //        WebElement clickingSendButton= driver.findElement (xpath ("//*[@alt=\"send_doubts\"]"));
 //        clickingSendButton.click();
 
-        // Clicking the Follow Icon
-
-        Thread.sleep (3000);
-        WebElement followIcon = driver.findElement (By.xpath ("//*[@alt=\"followed\"]"));
-        WebElement unfollowIcon = driver.findElement (By.xpath ("//*[@alt=\"followed\"]"));
-
-        if (followIcon != null) {
-            followIcon.click ();
-            System.out.println ("Successfully clicked Follow");
-        } else if (unfollowIcon != null) {
-            unfollowIcon.click ();
-            System.out.println ("Successfully clicked Unfollow");
-        } else {
-            System.out.println ("Could not find follow or unfollow icon element.");
-        }
-
-        // Clicking the Answer Icon
-
-        WebElement clickingAnswericon = driver.findElement (id ("comments-icon"));
-        clickingAnswericon.click ();
-
-        // Entering the text
-
-        WebElement Enteringtext = driver.findElement (id ("teatx-area-id"));
-        Enteringtext.sendKeys ("Thanks");
+//        // Clicking the Follow Icon
+//
+//        Thread.sleep (3000);
+//        WebElement followIcon = driver.findElement (By.xpath ("//*[@alt=\"followed\"]"));
+//        WebElement unfollowIcon = driver.findElement (By.xpath ("//*[@alt=\"followed\"]"));
+//
+//        if (followIcon != null) {
+//            followIcon.click ();
+//            System.out.println ("Successfully clicked Follow");
+//        } else if (unfollowIcon != null) {
+//            unfollowIcon.click ();
+//            System.out.println ("Successfully clicked Unfollow");
+//        } else {
+//            System.out.println ("Could not find follow or unfollow icon element.");
+//        }
+//
+//        // Clicking the Answer Icon
+//
+//        WebElement clickingAnswericon = driver.findElement (id ("comments-icon"));
+//        clickingAnswericon.click ();
+//
+//        // Entering the text
+//
+//        WebElement Enteringtext = driver.findElement (id ("teatx-area-id"));
+//        Enteringtext.sendKeys ("Thanks");
 
 //        // Clicking the Send button
 //
@@ -215,59 +213,59 @@ public class AllDoubts {
 //        WebElement clickingLikeButton = driver.findElement (xpath ("//*[@alt=\"liked\"]"));
 //        clickingLikeButton.click ();
 
-        // Clicking the close Button
-
-        Thread.sleep (5000);
-        WebElement clickingCloseButton = driver.findElement (xpath ("(//img[@alt=\"Close\"])[3]"));
-        clickingCloseButton.click ();
-
-        // Clicking The share Icon
-
-        Thread.sleep (5000);
-        WebElement clickingshareicon = driver.findElement (xpath ("//*[@alt=\"share\"]"));
-        clickingshareicon.click ();
-
-        //click the copy link in the share popup
-
-        Thread.sleep (3000);
-        WebElement Copy_link = driver.findElement (xpath ("//span[text()='COPY LINK']"));
-        Copy_link.click ();
-
-        //Clicking the cancel button in the in share popup
-
-        Thread.sleep (5000);
-        WebElement Cancel_button = driver.findElement (xpath ("//span[text()='Cancel']"));
-        Cancel_button.click ();
-
-        // Clicking The three Dots in the doubts
-
-        Thread.sleep (5000);
-        WebElement clickingThreeDots = driver.findElement (xpath ("//*[@class=\"doubts-Menu\"]"));
-        clickingThreeDots.click ();
-
-        // Clicking The report Icon in the dropdown
-
-        Thread.sleep (5000);
-        WebElement clickingReport = driver.findElement (xpath ("//*[text()='Report']"));
-        clickingReport.click ();
-
-        //Click the wrong information radio button
-
-        Thread.sleep (5000);
-        WebElement Wrong_information_radio_button = driver.findElement (xpath ("//span[text()='Wrong Information']"));
-        Wrong_information_radio_button.click ();
-
-        //Entering the text inside the report text field
-
-        Thread.sleep (5000);
-        WebElement Enter_the_report = driver.findElement (name ("reportDescription"));
-        Enter_the_report.sendKeys ("Checking The text was Entering In The Report Text Field");
-
-        // Clicking The Cancel Button
-
-        Thread.sleep (5000);
-        Cancel_button = driver.findElement (xpath ("//span[text()='CANCEL']"));
-        Cancel_button.click ();
+//        // Clicking the close Button
+//
+//        Thread.sleep (5000);
+//        WebElement clickingCloseButton = driver.findElement (xpath ("(//img[@alt=\"Close\"])[3]"));
+//        clickingCloseButton.click ();
+//
+//        // Clicking The share Icon
+//
+//        Thread.sleep (5000);
+//        WebElement clickingshareicon = driver.findElement (xpath ("//*[@alt=\"share\"]"));
+//        clickingshareicon.click ();
+//
+//        //click the copy link in the share popup
+//
+//        Thread.sleep (3000);
+//        WebElement Copy_link = driver.findElement (xpath ("//span[text()='COPY LINK']"));
+//        Copy_link.click ();
+//
+//        //Clicking the cancel button in the in share popup
+//
+//        Thread.sleep (5000);
+//        WebElement Cancel_button = driver.findElement (xpath ("//span[text()='Cancel']"));
+//        Cancel_button.click ();
+//
+//        // Clicking The three Dots in the doubts
+//
+//        Thread.sleep (5000);
+//        WebElement clickingThreeDots = driver.findElement (xpath ("//*[@class=\"doubts-Menu\"]"));
+//        clickingThreeDots.click ();
+//
+//        // Clicking The report Icon in the dropdown
+//
+//        Thread.sleep (5000);
+//        WebElement clickingReport = driver.findElement (xpath ("//*[text()='Report']"));
+//        clickingReport.click ();
+//
+//        //Click the wrong information radio button
+//
+//        Thread.sleep (5000);
+//        WebElement Wrong_information_radio_button = driver.findElement (xpath ("//span[text()='Wrong Information']"));
+//        Wrong_information_radio_button.click ();
+//
+//        //Entering the text inside the report text field
+//
+//        Thread.sleep (5000);
+//        WebElement Enter_the_report = driver.findElement (name ("reportDescription"));
+//        Enter_the_report.sendKeys ("Checking The text was Entering In The Report Text Field");
+//
+//        // Clicking The Cancel Button
+//
+//        Thread.sleep (5000);
+//        Cancel_button = driver.findElement (xpath ("//span[text()='CANCEL']"));
+//        Cancel_button.click ();
 
 //        // Click the report button
 //
@@ -283,39 +281,39 @@ public class AllDoubts {
 //        clickingdoubtname.click ();
 //        System.out.println ("SucessFully clicked");
 
-        int numberoftimeclickingarrow = 2;
+//        int numberoftimeclickingarrow = 2;
+//
+//        for (int i = 0; i < numberoftimeclickingarrow; i++) {
+//            try {
+//                Thread.sleep (5000);
+//                WebElement clickingdoubtname = driver.findElement (By.xpath ("(//*[@class=\"anticon anticon-right\"])[2]"));
+//                clickingdoubtname.click ();
+//                System.out.println ("Successfully clicked on element " + (i + 1) + " time(s).");
+//            } catch (Exception e) {
+//                System.out.println ("Failed to find or click element: " + e.getMessage ());
+//            }
+//        }
 
-        for (int i = 0; i < numberoftimeclickingarrow; i++) {
-            try {
-                Thread.sleep (5000);
-                WebElement clickingdoubtname = driver.findElement (By.xpath ("(//*[@class=\"anticon anticon-right\"])[2]"));
-                clickingdoubtname.click ();
-                System.out.println ("Successfully clicked on element " + (i + 1) + " time(s).");
-            } catch (Exception e) {
-                System.out.println ("Failed to find or click element: " + e.getMessage ());
-            }
-        }
-
-        // Scrolling the doubt page
-
-        int numberoftimesscroll = 20;
-
-        for (int i = 0; i < numberoftimesscroll; i++) {
-            try {
-                Thread.sleep (3000);
-                JavascriptExecutor jse = (JavascriptExecutor) driver;
-                jse.executeScript ("window.scrollTo(0,document.body.scrollHeight)");
-                System.out.println ("Successfully scroll The page " + (i + 1) + " time(s).");
-            } catch (Exception scroll) {
-                System.out.println ("Failed to Scroll : " + scroll.getMessage ());
-            }
-        }
-
-        // clicking the Float button
-
-        Thread.sleep (5000);
-        WebElement clickingFloatButton = driver.findElement (xpath ("//*[@class=\"anticon anticon-vertical-align-top\"]"));
-        clickingFloatButton.click ();
+//        // Scrolling the doubt page
+//
+//        int numberoftimesscroll = 20;
+//
+//        for (int i = 0; i < numberoftimesscroll; i++) {
+//            try {
+//                Thread.sleep (3000);
+//                JavascriptExecutor jse = (JavascriptExecutor) driver;
+//                jse.executeScript ("window.scrollTo(0,document.body.scrollHeight)");
+//                System.out.println ("Successfully scroll The page " + (i + 1) + " time(s).");
+//            } catch (Exception scroll) {
+//                System.out.println ("Failed to Scroll : " + scroll.getMessage ());
+//            }
+//        }
+//
+//        // clicking the Float button
+//
+//        Thread.sleep (5000);
+//        WebElement clickingFloatButton = driver.findElement (xpath ("//*[@class=\"anticon anticon-vertical-align-top\"]"));
+//        clickingFloatButton.click ();
 
         // Clicking The Googleplay Button
 
@@ -358,12 +356,12 @@ public class AllDoubts {
         // Clicking The Search On the Left side Filter
 
         Thread.sleep (5000);
-        clickingSearch = driver.findElement (xpath ("//*[@name=\"comments2\"]"));
+        WebElement clickingSearch = driver.findElement (xpath ("//*[@name=\"comments2\"]"));
         clickingSearch.sendKeys ("Mock");
 
         // Pressing The KeyBoard Action
 
-        robot = new Robot ();
+        Robot robot = new Robot ();
 
         // clicking The Enter Button
 
@@ -455,26 +453,39 @@ public class AllDoubts {
 
         WebElement printingTheAllFilter = driver.findElement (By.id ("doubt-sidebar-body"));
         String allText = printingTheAllFilter.getText ();
-        System.out.println (allText);
+        System.out.println (" All Course List In Filter ---> " + allText);
 
-        List<WebElement> courseLists = driver.findElements (By.xpath ("//div[@class='courselist']"));
+        String[] courseName = {allText};
+
+        System.out.println ("Entered To Verify ");
+
+        List<WebElement> courseLists = driver.findElements (By.xpath ("//div[@class='doubt-left-sidebar-course-body']"));
 
         Set<String> uniqueCourse = new HashSet<> ();
+
         int uniqueCourseCount = 0;
 
+        System.out.println ("Entering Loop");
+
         for (WebElement courseList : courseLists) {
+
             String actualCourseList = courseList.getText ();
 
-            if (uniqueCourse.add (actualCourseList)) {
-                // Only do this if it is a unique course
-                if (allText.contains (actualCourseList)) {
+            System.out.println ("Printing Course Name ---> " + actualCourseList);
+
+            if (uniqueCourse.contains (actualCourseList)) {
+                System.out.println ("Duplicate found --> " + actualCourseList);
+            } else {
+                uniqueCourse.add (actualCourseList);
+                if (Arrays.asList (courseName).contains (actualCourseList)) {
                     uniqueCourseCount++;
                     System.out.println ("Found: " + uniqueCourseCount + "-->" + actualCourseList);
                 }
             }
-
-
         }
+        System.out.println ("Total unique CourseName found: " + uniqueCourseCount);
+//            Assert.assertEquals (uniqueCourseCount, courseName.length);
     }
+
 }
 
